197d5293d30197a27429865262d2e9d86a642245
AviNagosa <avinagossa91@gmail.com>
2022-03-19 19:38:01 +0200

    db connect

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
 const express = require('express');
+const { Result } = require('express-validator');
+const mysql = require('mysql');
+const { reset } = require('nodemon');
+
+//Create connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "nodemydb"
+});
+
+//Connect
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 const app = express();
+
+//Create db
+app.get('/createdb', (req,res)=> {
+    let sql = 'CREATE DATABASE nodemydb';
+    db.query(sql, (err, result)=> {
+        if(err) throw err;
+        console.log("Result: " + result);
+        res.send('database created...');
+    });
+});
+
+//Create table
+app.get('/createtable', (req,res)=> {
+    let sql = 'CREATE TABLE Item(ItemName varchar(255), Cost int)';
+    db.query(sql, (err, result)=> {
+        if(err) throw err;
+        console.log("Result: " + result);
+        res.send('Item table created...');
+    });
+});
+
 
 app.get('/', (req,res)=> {
     res.sendFile('./views/index.html',{root:__dirname})
@@ -9,7 +48,8 @@ app.get('/', (req,res)=> {
     res.sendFile('./views/search.html',{root:__dirname})
 })
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT,()=>{
     console.log('Server runs');
